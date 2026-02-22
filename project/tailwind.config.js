a8ee18f1fede7db4aae544b435270545a1bbb635
ronitrehal-cpu <ronitrehal@gmail.com>
2026-02-22 22:28:56 +1100

    Add files via upload

---
+++ project/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+        serif: ['Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
